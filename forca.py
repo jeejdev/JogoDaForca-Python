2b717c8c4c7b91267154d8b4d6dd74492363a18c
jeejinf <jeejinf>
2022-11-23 09:11:32 -0300

    Criado o 'requirements.txt' + criação função escolhe

--- forca.py
+++ forca.py
@@ -8,7 +8,7 @@ import requests
 letrasCertas = letrasErradas = ""
 
 listaPalavras = requests.get('https://www.ime.usp.br/~pf/dicios/br-sem-acentos.txt').text.lower().split()
-palavraEscolhida = random.choice(listaPalavras)
+
 
 def chute(a):
     alfabeto = list(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
@@ -16,4 +16,6 @@ def chute(a):
     while a not in alfabeto:
         a = input('Você não digitou uma letra, tente novamente!')
 
-print(palavraEscolhida)
+def escolhe():
+    palavraEscolhida = random.choice(listaPalavras)
+    return palavraEscolhida
